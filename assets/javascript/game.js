d07f1b097d532b1f2a2b9c492e3d2859e97ef425
William R Poulson <jessica.spildener@gmail.com>
2017-08-07 11:25:45 -0400

    game completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,54 +17,63 @@ $(document).ready(function() {
     var totalLosses = 0;
     var totalScore = 0;
 
+    var resetOutcome = function() {
+        $("#outcome").text("");
+    }
+
     var resetGame = function() {
-       	randomNumber = Math.floor((Math.random() * 120) + 1);
-       	$("#numberToGuess").text(randomNumber);
+        randomNumber = Math.floor((Math.random() * 120) + 1);
+        $("#numberToGuess").text(randomNumber);
         redGem = gemNumber();
         blueGem = gemNumber();
         yellowGem = gemNumber();
         greenGem = gemNumber();
         totalScore = 0;
         $("#totalScore").text(totalScore);
+
     }
 
-var checkWinLoss = function() {
-    if (totalScore === randomNumber) {
-        totalWins++;
-        console.log("win");
-        $("#totalWin").text(totalWins);
-        resetGame();
-    } else if (totalScore < randomNumber) {
-    	console.log("continue");
-    } else {
-        totalLosses++;
-        console.log("loss");
-        $("#totalLosses").text(totalLosses);
-        resetGame();
+    var checkWinLoss = function() {
+        if (totalScore === randomNumber) {
+            totalWins++;
+            console.log("win");
+            $("#totalWin").text(totalWins);
+            $("#outcome").text("You win!!");
+            resetGame();
+            setTimeout(resetOutcome, 3000);
+        } else if (totalScore < randomNumber) {
+            console.log("continue");
+        } else {
+            totalLosses++;
+            console.log("loss");
+            $("#totalLosses").text(totalLosses);
+            $("#outcome").text("You lose.");
+            resetGame();
+            setTimeout(resetOutcome, 3000);
+        }
     }
-}
 
-$("#redGem").on("click", function() {
-    totalScore = totalScore + redGem;
-    console.log(totalScore);
-    $("#totalScore").text(totalScore);
-    checkWinLoss();
-});
- $("#blueGem").on("click", function() {
-    totalScore = totalScore + blueGem;
-    $("#totalScore").text(totalScore);
-    checkWinLoss();
-});
- $("#yellowGem").on("click", function() {
-    totalScore = totalScore + yellowGem;
-    $("#totalScore").text(totalScore);
-    checkWinLoss();
-});
- $("#greenGem").on("click", function() {
-    totalScore = totalScore + greenGem;
-    $("#totalScore").text(totalScore);
-    checkWinLoss();
-});
+    $("#redGem").on("click", function() {
+        totalScore = totalScore + redGem;
+        console.log(totalScore);
+        $("#totalScore").text(totalScore);
+        checkWinLoss();
+    });
+    $("#blueGem").on("click", function() {
+        totalScore = totalScore + blueGem;
+        $("#totalScore").text(totalScore);
+        checkWinLoss();
+    });
+    $("#yellowGem").on("click", function() {
+        totalScore = totalScore + yellowGem;
+        $("#totalScore").text(totalScore);
+        checkWinLoss();
+    });
+    $("#greenGem").on("click", function() {
+        totalScore = totalScore + greenGem;
+        $("#totalScore").text(totalScore);
+        checkWinLoss();
+    });
 
 
 
